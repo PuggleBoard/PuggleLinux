6845b698cecc0dbfe66c8b49b79eef9180031444
yapatel525 <yapatel@gatech.edu>
2013-05-23 22:45:16 -0400

    Adding basic necessities.

---
+++ client/src/main.c
@@ -0,0 +1,36 @@
+/*
+	 -------------------------------------------------------------------------
+
+	 This file is part of the Puggle Data Conversion and Processing System
+	 Copyright (C) 2013 Puggle
+
+	 -------------------------------------------------------------------------
+
+	 Written in 2013 by: Yogi Patel <yapatel@gatech.edu>
+
+	 Additional contributions by Chris Micali of Sage Devices
+
+	 To the extent possible under law, the author(s) have dedicated all copyright
+	 and related and neighboring rights to this software to the public domain
+	 worldwide. This software is distributed without any warranty.
+
+	 You should have received a copy of the CC Public Domain Dedication along with
+	 this software. If not, see <http://creativecommons.org/licenses/by-sa/3.0/legalcode>.
+	 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+#include <signal.h>
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+#include <pruss_intc_mapping.h>
+
+int main(int argc,char *argv[]) {
+}
